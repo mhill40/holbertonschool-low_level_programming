b8de7673b15cc13f1ebdfa7b15ebbfdcc6ac81d1
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-23 17:12:07 -0500

    Add files via upload
    mhill40 hello world c 0x05 and 0x00

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{       
+                // int double float //
+                int dogs = 6;
+                //presicion -- how much data variable can hold aka 999.9999 or 4
+                //
+
+                printf("%i %f %f\n", 1, 1.1111, 1.1111F); 
+     
+                //float ..it becomes double. fun with math. now for a hello world
+
+                printf(""Hello World! This is Matt Hs first C program!"\n");
+
+                return 0;
+}                                   
