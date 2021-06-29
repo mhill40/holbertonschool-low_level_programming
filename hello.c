63469b9f837cfc860ce0e25a1cf79186363df787
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-29 13:14:51 -0500

    Update hello.c
    mhill40

--- hello.c
+++ hello.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
+#include 
 
 int main()
-{       
-                // int double float //
-                int dogs = 6;
-                //presicion -- how much data variable can hold aka 999.9999 or 4
-                //
-
-                printf("%i %f %f\n", 1, 1.1111, 1.1111F); 
-     
-                //float ..it becomes double. fun with math. now for a hello world
-
-                printf(""Hello World! This is Matt Hs first C program!"\n");
+{
+                printf('Hello World! This is Matt Hs first C program'\n);
 
                 return 0;
 }                                   
