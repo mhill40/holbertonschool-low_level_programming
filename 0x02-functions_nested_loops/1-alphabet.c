4dde79cf427dd154a484377f9b196462aabe85dc
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-29 00:40:42 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
-cat 1-main.c
 #include "holberton.h"
 #include <stdio.h>
 #include <stddef.h>
 
 int main() {
 char x;
-for (x = 'A'; x <= 'Z'; ++x);
+for (x = 'A'; x <= 'Z'; x++)
 printf(%c ", c);
 return 0;
 }
