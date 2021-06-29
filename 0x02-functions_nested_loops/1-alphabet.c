a9999db1f287db4953c21ad2a0fbdf7b301adba8
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-29 00:49:28 -0500

    Update 1-alphabet.c
    mhill40

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,11 @@
 #include <stddef.h>
 
 int main()
-{
 char x;
 for (x = 'A'; x <= 'Z'; x++)
+{
 printf(%x ", x);
-return 0;
+return (0);
 }
+       
+       // it shoud be as simple as printf_alphabet > (arrayAlphabet[]) to put them in an array form output. i dont understand why the loop isnt working
