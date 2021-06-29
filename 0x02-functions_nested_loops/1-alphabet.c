3b1322acc810a1091b7b22739e6ebae2e70004e5
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-29 00:57:59 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,19 @@
+cat 1-main.c
 #include "holberton.h"
 #include <stdio.h>
 #include <stddef.h>
 
-int main()
-char x;
-for (x = 'A'; x <= 'Z'; x++)
+int main (void)
 {
-printf("%x ", x);
-return (0);
+       print_alphabet(1);
+       return (0);
 }
+//int main()
+//char x;
+//for (x = 'A'; x <= 'Z'; x++)
+//{
+//printf("%x ", x);
+//return (0);
+//}
        
        // it shoud be as simple as printf_alphabet > (arrayAlphabet[]) to put them in an array form output. i dont understand why the loop isnt working
