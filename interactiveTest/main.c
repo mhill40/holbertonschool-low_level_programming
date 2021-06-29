b7e93e085714abc10a609d45acbf5b3d512675cb
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-29 04:32:41 -0500

    Create main.c
    Making sure I understand printing and variables mhill40

---
+++ interactiveTest/main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdlib.h>
+
+int main ()
+{ 
+char color [20]
+char pluralNoun[20]
+char celebrity[20]
+
+printf("enter a color: ");
+scanf("%s", color);
+  
+//that asks user and has user inpu a vaue printf("Enter a plural nount: ");
+scanf("%s, pluralNoun);
+printf("Enter a celebrity";
+scanf("%s, celebrity);
+// you are passing with scanf the value to the parameter or char that has a limit of 20 length
+
+printf("Roses are %s\n", color); 
+print("%s are blue\n", plural noun); 
+printf("I love %s\n, celebrity);
+
+return (0);
+}
