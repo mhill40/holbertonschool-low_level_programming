7317100eba0df832a069a19780341362be4ba303
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-29 05:01:33 -0500

    Update InteractiveInputPoem.c
    Mhill40

--- InteractiveInputPoem.c
+++ InteractiveInputPoem.c
@@ -12,14 +12,14 @@ int main ()
   printf("enter a color: ");
   scanf("%s", color);
   //that asks user and has user inpu a vaue
-  printf("Enter a plural nount: ");
+  printf("Enter a plural noun: ");
   scanf("%s, pluralNoun);
   printf("Enter a celebrity");
   scanf("%s, celebrity);
   // you are passing with scanf the value to the parameter or char that has a limit of 20 length
   
   printf("Roses are %s\n", color);
-  print("%s are blue\n", plural noun);
+  print("%s are blue\n", pluralNoun);
   printf("I love %s\n, celebrity);
          
          // %s is string %d decimal %i interger
