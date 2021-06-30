18574e7b419b800a0af0c7dfb8a937ad14301e01
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-30 00:00:59 -0500

    Add files via upload
    mhill40

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf((if "last digit of" %d \n %s"is" %s"less than 6 and not 0")\n);
+	return (0);
+}
