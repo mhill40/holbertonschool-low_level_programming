1c3eb8ba864331ac4ef832704d94ff5111a24455
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-30 00:46:11 -0500

    Add files via upload
    mhill40

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() 
+
+   int num;{
+   num=0;
+   for(num=0; num >= 10; num++);
+    printf("%d, num");
+   return 0;
+   }
+}
