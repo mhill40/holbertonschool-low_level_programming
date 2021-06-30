b0a515911c2404b3779ebf408008584e4a3c6edf
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-30 00:34:36 -0500

    Add files via upload
    mhill40 0x01

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char ltr;
+
+    printf("The lowercase alphabet without q or e is: \n");
+    for(ltr='a'; ltr='z'; ltr++)
+    {
+        printf(["%C\n", ltr)];
+        if ltr[i] = q{
+            pop ltr[q];
+        }
+        else if ltr[i] = e;{
+            pop ltr[e];
+        }
+        else{
+    return 0;
+        }
+}
