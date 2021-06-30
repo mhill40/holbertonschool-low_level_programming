af703aab9b4857f00dcefd05be93ae608a0d46da
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-30 00:09:46 -0500

    Create 2-print_alphabet.c
    mhill40

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char ltr;
+
+    printf("The Alphabet is: \n");
+    for(ltr='a'; ltr='z'; ltr++)
+    {
+        printf("%c\n", ltr);
+    }
+
+    return 0;
+}
