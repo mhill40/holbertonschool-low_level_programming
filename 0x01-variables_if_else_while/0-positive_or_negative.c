fa55a4f65107fce335a9f205e593b322b467d393
Matt <58513192+mhill40@users.noreply.github.com>
2021-06-29 23:59:52 -0500

    Update 0-positive_or_negative.c
    mhill40

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int 444;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	printf(%d, n);
 	return (0);
 }
