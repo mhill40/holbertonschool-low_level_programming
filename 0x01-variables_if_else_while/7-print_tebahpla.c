a84f8432b3339d88ad203af7406a24b5c29a41d5
Matt <58513192+mhill40@users.noreply.github.com>
2021-07-07 02:13:21 -0500

    Create 7-print_tebahpla.c
    mhill40

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+/**
+ * main is the enter point
+ * print lowercase alphabet in reverse followed by new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char lower;
+
+        for (lower = 'z'; lower >= 'a'; lower--)
+                putchar(lower);
+                putchar('\n');
+
+        return (0);
+}
