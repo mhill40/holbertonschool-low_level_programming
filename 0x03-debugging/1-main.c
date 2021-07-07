15b6f3c59292b7c2b9a439c065f4fcca94680813
Matt <58513192+mhill40@users.noreply.github.com>
2021-07-07 01:46:50 -0500

    Update 1-main.c
    mhill40 I commented out the parts that created the loop and avoided the infinite loop. I now understand how to make and fix infinite loops

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - causes an infinite loop. i have to figure out what to comment out...
 * Return: 0
 */
 
 int main(void)
 {
-        int i;
+   /*     int i;
 
         printf("Infinite loop incoming :(\n");
 
         i = 0;
 
-        while (i < 10)
+       while (i < 10)
         {
                 putchar(i);
         }
 
-        printf("Infinite loop avoided! \\o/\n");
+ */       printf("Infinite loop avoided! \\o/\n");
 
         return (0);
 }
