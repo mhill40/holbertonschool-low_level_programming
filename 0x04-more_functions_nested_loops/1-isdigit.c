7e8f12c593563e0f826763e27400539de79ffe29
Matt <58513192+mhill40@users.noreply.github.com>
2021-07-07 15:09:23 -0500

    Add files via upload
    mhill40

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main will check the code for Holberton School students.
+ *
+ * Return is Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
+}
