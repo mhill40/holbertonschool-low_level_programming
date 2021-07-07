9fd9a5c4fbdaee53405f806aeb2df65c2b5e87a8
Matt <58513192+mhill40@users.noreply.github.com>
2021-07-07 03:29:55 -0500

    Update 10-print_combo_2.c
    mhill40 independence day weekend / monday

--- 0x01-variables_if_else_while/10-print_combo_2.c
+++ 0x01-variables_if_else_while/10-print_combo_2.c
@@ -1,24 +1,26 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-/*
-//inside main, print 1-99 out in a single line separated with commas (,)
- //Return: (0) Success
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main is always entry point
+ * program that prints all possible combinations of 2 digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 99; x++)
+	for (x = 0; x < 100; x++)
 	{
-		putchar(x / 10 + '0');
-		putchar(x % 10 + '0');
-		if (x < 99)
+		putchar((x / 10) + '0');
+		putchar((x % 10) + '0');
+		if (x != 99)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
