84cb2d0672a53db606b764f2026097d08efa83db
Matt <58513192+mhill40@users.noreply.github.com>
2021-07-07 00:35:18 -0500

    Update holberton.h
    mhill40

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+        int x;
 
-        i = 98;
-        positive_or_negative(i);
+        x = 42;
+        positive_or_negative(x);
 
         return (0);
 }
