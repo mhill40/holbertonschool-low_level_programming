4093bbcb2435db41f26a4a8232cb4d69e24268ad
Matt <58513192+mhill40@users.noreply.github.com>
2021-07-07 14:59:43 -0500

    Add files via upload
    mhill40

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <main.h>
+#include <stdlib.h>
+#include <math.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
+// HOLBERTON_H != HOLBERTON_H
